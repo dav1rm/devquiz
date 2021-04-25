e06cd104658b517e99c6bdf9180dcafc2ccde2aa
Davi Rodrigues <davirodrigues8@gmail.com>
2021-04-25 11:49:12 -0300

    add level button widget

---
+++ lib/home/widgets/level_button/level_button_widget.dart
@@ -0,0 +1,63 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LevelButtonWidget extends StatelessWidget {
+  final String label;
+
+  LevelButtonWidget({
+    Key? key,
+    required this.label,
+  })   : assert(["Fácil", "Médio", "Difícil", "Perito"].contains(label)),
+        super(key: key);
+
+  final config = {
+    "Fácil": {
+      "color": AppColors.levelButtonFacil,
+      "borderColor": AppColors.levelButtonBorderFacil,
+      "fontColor": AppColors.levelButtonTextFacil,
+    },
+    "Médio": {
+      "color": AppColors.levelButtonMedio,
+      "borderColor": AppColors.levelButtonBorderMedio,
+      "fontColor": AppColors.levelButtonTextMedio,
+    },
+    "Difícil": {
+      "color": AppColors.levelButtonDificil,
+      "borderColor": AppColors.levelButtonBorderDificil,
+      "fontColor": AppColors.levelButtonTextDificil,
+    },
+    "Perito": {
+      "color": AppColors.levelButtonPerito,
+      "borderColor": AppColors.levelButtonBorderPerito,
+      "fontColor": AppColors.levelButtonTextPerito,
+    },
+  };
+
+  Color get color => config[label]!["color"]!;
+  Color get fontColor => config[label]!["fontColor"]!;
+  Color get borderColor => config[label]!["borderColor"]!;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 32,
+      width: 80,
+      decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(28),
+          border: Border.fromBorderSide(
+            BorderSide(color: borderColor),
+          )),
+      child: Center(
+        child: Text(
+          label,
+          style: GoogleFonts.notoSans(
+            color: fontColor,
+            fontSize: 13,
+          ),
+        ),
+      ),
+    );
+  }
+}
