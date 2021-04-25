53d942d0a437573377fe5b95c8f8fb2f9ed650ea
Davi Rodrigues <davirodrigues8@gmail.com>
2021-04-25 20:57:53 -0300

    use progress indicator widget

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -39,12 +40,7 @@ class QuizCardWidget extends StatelessWidget {
               ),
               Expanded(
                 flex: 1,
-                child: LinearProgressIndicator(
-                  value: 0.3,
-                  backgroundColor: AppColors.chartSecondary,
-                  valueColor:
-                      AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
-                ),
+                child: ProgressIndicatorWidget(value: 0.3),
               )
             ],
           )
